9aa9d8ebc3c331703b6973e77702cb697a706e41
vyura06 <vechorko.yura@gmail.com>
2022-10-15 15:18:12 +0300

    Update db

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,9 +1,9 @@
 /* eslint-disable strict */
 module.exports = {
-  host: "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
-  user: "ktfulspejaiilw",
-  pass: "4931e09acc03ccae82bb6c2c58a1e03391721e92c3f673d64fd9826c544dbb48",
-  dbname: "d33jqos7hgdc7",
+  host: "ec2-34-253-119-24.eu-west-1.compute.amazonaws.com",
+  user: "urptjxbtilcfqe",
+  pass: "6976659e672f1e464c38cf4a032a3d7ee92edcb1b54bdbbb5c7dd62a5ac215f0",
+  dbname: "del4ng8uelo3qg",
   port: 5432,
   ssl: { rejectUnauthorized: false }
 };
